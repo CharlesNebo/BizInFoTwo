74cfc6b59cd4403012ca4e515b2d2f133add33ef
Charles Nebo <n1nech00@students.oamk.fi>
2013-10-10 01:09:34 +0300

    BizInFoTwo
    UI Design of BizInFo Project

--- BizInFoTwo/ViewController.h
+++ BizInFoTwo/ViewController.h
@@ -7,7 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<MKMapViewDelegate>
+{  
+    IBOutlet UITextView *textView;
+    IBOutlet MKMapView *mapView;
+    IBOutlet UITextField *textField;
+    IBOutlet UIButton *Go;
+    CLLocationManager *locationManager;
+    CLLocation *currentLocation;
 
+}
+@property (nonatomic, retain) IBOutlet MKMapView *mapView;
+@property (nonatomic, retain) IBOutlet UITextField *textField;
+@property (nonatomic, retain) IBOutlet UITextView *textView;
+@property (nonatomic, retain) IBOutlet UIButton *Go;
+@property (nonatomic, retain) CLLocation *currentLocation;
+-(IBAction)btnGo;
 @end
--- BizInFoTwo/ViewController.m
+++ BizInFoTwo/ViewController.m
@@ -13,11 +13,16 @@
 @end
 
 @implementation ViewController
+@synthesize Go, textField, textView, mapView, currentLocation;
+
 
 - (void)viewDidLoad
 {
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +30,18 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(IBAction)btnGo{
+    
+    textField = [UITextField]
+    [ textField resignFirstResponder];
+    
+
+        
+        
+    
+}
+
 
 @end
